7d5b6f96cb3565cbf1064c999181ca07d7ae71ab
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-03-17 22:49:42 +0100

    add changes into get_curren_tweather function

--- weather.py
+++ weather.py
@@ -5,14 +5,13 @@ import os
 
 load_dotenv() #andiamo a prendere le vcar di ambiente dal file .env
 
-def get_current_weather(city="Latina"): #andimao a dare di default una citta
-    weather_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("key")}&q={city}&units=metric'
-
-    req_data = requests.get(weather_url).json #aniamo a chidere al server le info con l'url composto sopra
-    return req_data #le andiamo a ritornare
-
-
-
+def get_current_weather(city='Latina'): #andiamo a dare di default una citta
+    try:
+      weather_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("key")}&q={city}&units=metric'
+      req_data = requests.get(weather_url).json() #aniamo a chidere al server le info con l'url composto sopra
+      return req_data #le andiamo a ritornare
+    except Exception as error:
+        print(error) 
 
 if __name__ == "__main__": #se esguiamo direttamente, dal file, il codice eseguiremo una versione in console
     city = input("\nEnter the city: \n") 
