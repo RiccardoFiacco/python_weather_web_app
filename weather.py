07d05fe685b86b8754eedbd1ab5d8b1eafb8b388
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-03-17 17:25:23 +0100

    add the main function

--- weather.py
+++ weather.py
@@ -1,1 +1,9 @@
 from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv() #andiamo a prendere le vcar di ambiente dal file .env
+
+def get_current_weather(city="Latina"): #andimao a dare di default una citta
+    print(city)
